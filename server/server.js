8a64a5a82e0225b45a5bb586baac47fc38087d02
harshloya24 <harshloya2004@gmail.com>
2026-04-03 23:31:56 +0530

    Refactor server to use dynamic port and export db
    Updated server to use environment variable for port and export db.

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
 const express = require('express')
 const cors = require('cors')
-const {connectDB} = require('./db') 
+const {connectDB} = require('./db')
 const authRoutes = require('./routes/authRoutes')
 const jobRoutes = require('./routes/jobRoutes')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
 app.use('/', authRoutes)
 app.use('/', jobRoutes)
 
 let db = null
 
 const startServer = async () => {
-  try {
-    db = await connectDB()
+  db = await connectDB()
 
-   app.listen(5000, () => {
-  console.log('Server running at http://localhost:5000/')
-})
-  } catch (error) {
-    console.log('DB Error:', error.message)
-    process.exit(1)
-  }
+  const PORT = process.env.PORT || 5000
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 }
 
 startServer()
