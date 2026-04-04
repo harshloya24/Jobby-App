f8be8cf402682f4d07a2bb157a55631a78eddc85
harshloya24 <harshloya2004@gmail.com>
2026-04-04 13:30:36 +0530

    Update jobController.js

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,48 +1,67 @@
 const { getDB } = require('../db')
 
-
 const getJobs = async (req, res) => {
-  const db = getDB()
+  try {
+    const db = getDB()
 
-  const {
-    search = '',
-    employment_type = '',
-    min_package = '',
-  } = req.query
+    const {
+      search = '',
+      employment_type = '',
+      min_package = '',
+    } = req.query
 
-  let query = `
-    SELECT * FROM jobs
-    WHERE title LIKE ?
-  `
+    let query = `
+      SELECT * FROM jobs
+      WHERE title LIKE ?
+    `
 
-  const params = [`%${search}%`]
+    const params = [`%${search}%`]
 
-  if (employment_type) {
-    query += ` AND employment_type = ?`
-    params.push(employment_type)
+    
+    const employmentTypeMap = {
+      FULLTIME: 'Full Time',
+      PARTTIME: 'Part Time',
+      FREELANCE: 'Freelance',
+      INTERNSHIP: 'Internship',
+    }
+
+    if (employment_type) {
+      query += ` AND employment_type = ?`
+      params.push(employmentTypeMap[employment_type])
+    }
+
+    
+    if (min_package) {
+      query += `
+        AND CAST(REPLACE(package_per_annum, ' LPA', '') AS INTEGER) >= ?
+      `
+      params.push(parseInt(min_package) / 100000)
+    }
+
+    const jobs = await db.all(query, params)
+
+    res.send({ jobs })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error: 'Internal Server Error' })
   }
-
-  if (min_package) {
-    query += ` AND package_per_annum LIKE ?`
-    params.push(`${min_package}%`)
-  }
-
-  const jobs = await db.all(query, params)
-
-  res.send({ jobs })
 }
 
-
 const getJobDetails = async (req, res) => {
-  const db = getDB()
-  const { id } = req.params
+  try {
+    const db = getDB()
+    const { id } = req.params
 
-  const job = await db.get(
-    `SELECT * FROM jobs WHERE id = ?`,
-    [id]
-  )
+    const job = await db.get(
+      `SELECT * FROM jobs WHERE id = ?`,
+      [id]
+    )
 
-  res.send({ job })
+    res.send({ job })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error: 'Internal Server Error' })
+  }
 }
 
 module.exports = { getJobs, getJobDetails }
