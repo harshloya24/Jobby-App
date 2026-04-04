4b0c30320110f85a4490adb3475300d305eda3d9
harshloya24 <harshloya2004@gmail.com>
2026-04-04 12:08:44 +0530

    Update jobController.js

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,9 +1,14 @@
-const {getDB} = require('../db')
+const { getDB } = require('../db')
+
 
 const getJobs = async (req, res) => {
   const db = getDB()
 
-  const {search = '', employment_type = '', min_package = ''} = req.query
+  const {
+    search = '',
+    employment_type = '',
+    min_package = '',
+  } = req.query
 
   let query = `
     SELECT * FROM jobs
@@ -18,27 +23,26 @@ const getJobs = async (req, res) => {
   }
 
   if (min_package) {
-    query += `
-      AND CAST(REPLACE(package_per_annum, ' LPA', '') AS INTEGER) >= ?
-    `
-    params.push(min_package)
+    query += ` AND package_per_annum LIKE ?`
+    params.push(`${min_package}%`)
   }
 
   const jobs = await db.all(query, params)
 
-  res.send({jobs})
+  res.send({ jobs })
 }
+
 
 const getJobDetails = async (req, res) => {
   const db = getDB()
-  const {id} = req.params
+  const { id } = req.params
 
   const job = await db.get(
     `SELECT * FROM jobs WHERE id = ?`,
     [id]
   )
 
-  res.send({job})
+  res.send({ job })
 }
 
-module.exports = {getJobs, getJobDetails}
+module.exports = { getJobs, getJobDetails }
